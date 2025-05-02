5b0d8c2da9f30e29fa2dd678fdf374b7daf3b0ae
Nikitas Konstantopoulos <nikitaskonstantopoulos07@gmail.com>
2025-05-02 16:49:28 +0200

    a

---
+++ EF-Core/Program.cs
@@ -0,0 +1,9 @@
+﻿using EF-Core.Models;
+
+using var context = new StudentDbContext();
+
+Console.WriteLine("List of Students:");
+foreach (var student in context.Students)
+{
+    Console.WriteLine($"{student.Id}: {student.Name} - {student.Email} ({student.EnrollmentDate.ToShortDateString()})");
+}
